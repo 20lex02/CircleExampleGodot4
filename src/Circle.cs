07cef0a05915a0ab932a29fd28c2c29593c96d4d
Alexandre Laroche <lalochea@gmail.com>
2022-09-28 10:39:18 -0400

    Add ability to tile textures

--- src/Circle.cs
+++ src/Circle.cs
@@ -4,15 +4,15 @@ using System;
 [Tool]
 public partial class Circle : Polygon2D
 {
-	[Export]
-	public int PointCount {
+	[Export(PropertyHint.Range, "0,4294967295")]
+	public uint PointCount {
 		get => _pointCount;
 		set {
 			_pointCount = value;
 			Redraw();
 		}
 	}
-	private int _pointCount = 100;
+	private uint _pointCount = 100;
 
 	[Export]
 	public float Radius {
@@ -23,8 +23,18 @@ public partial class Circle : Polygon2D
 			ResizeTexture();
 		}
 	}
-
 	private float _radius = 10;
+
+	[Export(PropertyHint.Range, "1,4294967295")]
+	public uint Tile {
+		get => _tile;
+		set {
+			_tile = value;
+			ResizeTexture();
+		}
+	}
+	private uint _tile = 1;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -38,7 +48,7 @@ public partial class Circle : Polygon2D
 
 	private void Redraw() 
 	{
-		GD.Print("Redrawing...");
+		GD.Print("Redrawing Polygon...");
 		var polygon = new Vector2[PointCount];
 		for (int i = 0; i < PointCount; i++)
 		{
@@ -57,7 +67,8 @@ public partial class Circle : Polygon2D
 	{
 		if (Texture is not null)
 		{
-			this.TextureScale = Texture.GetSize() / (Radius * 2);
+			GD.Print("Resizing Texture...");
+			this.TextureScale = Texture.GetSize() / (Radius * 2) * Tile;
 			this.TextureOffset = new Vector2(Radius, Radius);
 		}
 	}
